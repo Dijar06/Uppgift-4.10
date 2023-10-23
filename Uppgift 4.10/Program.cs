cfbebfd05acbb5bab0d3b6819c050e25688282c1
Dijar06 <dize06001@utb.vaxjo.se>
2023-10-23 15:09:55 +0200

    Add project files.

---
+++ Uppgift 4.10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Uppgift4._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många X per grupp?");
+            int x = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Hur många O per grupp?");
+            int o = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hur många O-mönster per rad?");
+            int om = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hur många rader?");
+            int rad = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rad; i++)
+            {
+                for (int j = 0; j < om; j++)
+                {
+                    for (int k = 0; k < x; k++)
+                    {
+                        Console.Write("X");
+                    }
+                    Console.Write("-");
+                    for (int l = 0; l < o; l++)
+                    {
+                        Console.Write("O");
+                    }
+                    Console.Write("-");
+                }
+                Console.WriteLine();
+            } 
+        }
+    }
+}
